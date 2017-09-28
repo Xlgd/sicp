311529ab1d0dfe2b0f303ad2bcbd0e0268bea648
Xlgd <1120496618@qq.com>
2017-09-28 11:30:26 +0800

    update

---
+++ Chapter_2/e2_6.scm
@@ -0,0 +1,22 @@
+(define zero (lambda (f) (lambda (x) x)))
+
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+
+(define one
+  (lambda (f)
+	(lambda (x)
+	  (f x))))
+
+(define two
+  (lambda (f)
+	(lambda (x)
+	  (f (f x)))))
+
+(define +
+  (lambda (m)
+	(lambda (n)
+	  (lambda (f)
+		(lambda (x)
+		  (m f (n f x)))))))
+
