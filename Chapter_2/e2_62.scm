f3a644c3da3a7276ab1e29426e3e34729e4c2666
Gaodong Liu <1120496618@qq.com>
2020-03-10 17:09:17 +0800

    update

--- Chapter_2/e2_62.scm
+++ Chapter_2/e2_62.scm
@@ -1,10 +1,11 @@
 #lang sicp
 
 (define (union-set set1 set2)
-  (if (or (null? set1) (null? set2))
-      '()
-      (let ((x1 (car set1)) (x2 (car set2)))
-        (cond ((= x1 x2) (cons x1 (union-set (cdr set1)
-                                            (cdr set2))))
-              ((< x1 x2) (cons x1 (union-set (cdr set1) set2)))
-              ((> x1 x2) (cons x2 (union-set set1 (cdr set2))))))))
+  (cond ((and (null? set1) (null? set2)) '())
+        ((null? set1) set2)
+        ((null? set2) set1)
+        (else
+         (let ((x1 (car set1)) (x2 (car set2)))
+           (cond ((= x1 x2) (cons x1 (union-set (cdr set1) (cdr set2))))
+                 ((< x1 x2) (cons x1 (union-set (cdr set1) set2)))
+                 ((> x1 x2) (cons x2 (union-set set1 (cdr set2)))))))))
