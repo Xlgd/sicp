451ce5e893dca0cff68ccd08cfa83012cd87b210
Gaodong Liu <1120496618@qq.com>
2020-03-19 18:07:16 +0800

    update

--- Chapter_3/digital-simulator.scm
+++ Chapter_3/digital-simulator.scm
@@ -25,10 +25,6 @@
     (or-gate c1 c2 c-out)
     'ok))
 
-(get-signal <wire>)
-(set-signal! <wire><new value>)
-(add-action! <wire><procedure of no arguments>)
-
 (define (inverter input output)
   (define (invert-input)
     (let ((new-value (logical-not (get-signal input))))
@@ -74,3 +70,36 @@
   (cond ((or (= x 1) (= y 1)) 1)
         (else 0)))
 
+(define (make-wire)
+  (let ((signal-value 0) (action-procedures '()))
+    (define (set-my-signal! new-value)
+      (if (not (= signal-value new-value))
+          (begin (set! signal-value new-value)
+                 (call-each action-procedures))
+          'done))
+    (define (accept-action-procedures! proc)
+      (set! action-procedures (cons proc action-procedures))
+      (proc))
+
+    (define (dispatch m)
+      (cond ((eq? m 'get-signal) signal-value)
+            ((eq? m 'set-signal!) set-my-signal!)
+            ((eq? m 'add-action!) accept-action-procedure!)
+            (else (error "Unknown operation -- WIRE" m))))
+    dispatch))
+
+(define (call-each procedures)
+  (if (null? procedures)
+      'done
+      (begin
+        ((car procedures))
+        (call-each (cdr procedures)))))
+
+(define (get-signal wire)
+  (wire 'get-signal))
+
+(define (set-signal! wire new-value)
+  ((wire 'set-signal!) new-value))
+
+(define (add-action! wire action-procedure)
+  ((wire 'add-action!) action-procedure))
