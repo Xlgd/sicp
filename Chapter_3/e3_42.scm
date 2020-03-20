67ee1870e80b2841c27af7435c5698b16b066fce
Gaodong Liu <1120496618@qq.com>
2020-03-21 00:34:19 +0800

    update

---
+++ Chapter_3/e3_42.scm
@@ -0,0 +1,24 @@
+#lang sicp
+
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+
+  (let ((protected (make-serializer)))
+    (let ((protected-withdraw (protected withdraw))
+          (protected-deposit (protected deposit)))
+      (define (dispatch m)
+        (cond ((eq? m 'withdraw) protected-withdraw)
+              ((eq? m 'deposit) protected-deposit)
+              ((eq? m 'balance) balance)
+              (else (error "Unknown request -- MAKE-ACCOUNT" m))))
+    dispatch)))
+
+;;Ben的过程不安全，会阻止单一对象进行并发
