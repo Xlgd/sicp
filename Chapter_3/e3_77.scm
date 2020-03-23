8384b699e4274906ff48d16276f05e1b115e2ad5
Gaodong Liu <1120496618@qq.com>
2020-03-23 09:27:30 +0800

    update

---
+++ Chapter_3/e3_77.scm
@@ -0,0 +1,14 @@
+#lang sicp
+
+(define (integral delayed-integrand initial-value dt)
+  (cons-stream initial-value
+               (let ((integrand (force delayed-integrand)))
+                 (if (stream-null? integrand)
+                     the-empty-stream
+                     (integral (stream-cdr integrand)
+                               (+ (* dt (stream-car integrand))
+                                  initial-value)
+                               dt)))))
+
+
+
---
+++ Chapter_3/e3_79.scm
@@ -0,0 +1,8 @@
+#lang sicp
+
+(define (general-solve-2nd f y0 dy0 dt)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (integral (delay ddy) dy0 dt))
+  (define ddy (stream-map f dy y))
+  y)
+
